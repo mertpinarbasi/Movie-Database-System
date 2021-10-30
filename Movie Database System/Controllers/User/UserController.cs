1305aaaee8e7558895a577a7d362a42643f6f392
mertpinarbasi <mpinarbasi35@gmail.com>
2021-10-30 23:57:32 +0300

    Project Initialization
    Project has been created and register form has been added.

---
+++ Movie Database System/Controllers/User/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Database_System.Controllers.User
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterUser( String name , String surname )
+        {
+            Console.WriteLine("name" + name);
+            // database eklemece
+            return Json(name);
+        }
+    }
+}
--- Movie Database System/Startup.cs
+++ Movie Database System/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 
 namespace Movie_Database_System
@@ -24,6 +25,13 @@ namespace Movie_Database_System
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddControllers();
+     
+            services.AddWebEncoders(o => {
+                o.TextEncoderSettings = new System.Text.Encodings.Web.TextEncoderSettings(UnicodeRanges.All);
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
