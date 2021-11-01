e9cb95e0736403fc165cf4ba998eb0b62ad4afc0
Türkay Leblebicioğlu <turkay7879@gmail.com>
2021-11-01 15:06:33 +0300

    Azure DB Connection
    + Test query in HomeController.cs

---
+++ CreatePersonTableTest.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[UserId] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(20) NOT NULL, 
+    [Surname] NVARCHAR(30) NOT NULL, 
+    [Username] NVARCHAR(20) NOT NULL, 
+    [Password] NVARCHAR(20) NOT NULL
+)
+
+INSERT INTO Person([UserId],[Name],[Surname],[Username],[Password])
+VALUES
+(1, 'Eda İrem', 'Deniz', 'iremdnz26', 'dnzirem123'),
+(2, 'Mert', 'Pınarbaşı', 'mert41', 'pnbmert65')
+GO
