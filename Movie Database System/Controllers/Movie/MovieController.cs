b40585166fbf199e525acc48eecb4b1da1e9887c
Eda İrem Deniz <64044153+iremdnz@users.noreply.github.com>
2021-11-16 00:27:22 +0300

    Director selection is added

--- Movie Database System/Controllers/Movie/MovieController.cs
+++ Movie Database System/Controllers/Movie/MovieController.cs
@@ -5,13 +5,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Movie_Database_System.Models;
 using System.IO;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Collections;
 
 namespace Movie_Database_System.Controllers.Movie
 {
     public class MovieController : Controller
     {
+        private IConfiguration _config;
+
+        public MovieController(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public IActionResult AddMovie()
         {
+            List<Director> directors = new List<Director>();
+            string sql = "SELECT Name, Surname, Age FROM  dbo.Director";
+            using (var connection = new SqlConnection(_config.GetValue<string>("ConnectionStrings:MovieAppDB").ToString()))
+            using (var command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        //Console.WriteLine("{0} {1} {2}", reader.GetString(0), reader.GetString(1), reader.GetInt32(2));
+                        
+                        directors.Add(new Director(reader.GetString(0), reader.GetString(1), reader.GetInt32(2)));
+                        
+                    }
+                    ViewData["DirectorList"] = directors;
+                }
+            }
+
             return View();
         }
 
---
+++ Movie Database System/Models/Director.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Database_System.Models
+{
+    public class Director
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int age { get; set; }
+
+        public Director(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+    }
+}
