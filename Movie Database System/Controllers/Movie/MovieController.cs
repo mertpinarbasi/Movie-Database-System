275d3b4a686fe77e8c88d267ad51c63bc0a041b6
Türkay <turkay7879@gmail.com>
2021-12-15 22:30:44 +0300

    Added a view to show newly added movie

--- Movie Database System/Controllers/Movie/MovieController.cs
+++ Movie Database System/Controllers/Movie/MovieController.cs
@@ -82,8 +82,6 @@ namespace Movie_Database_System.Controllers.Movie
             {
                 try
                 {
-
-
                     movies.Add(new Models.Movie(searchMovieReader.GetInt32(0), searchMovieReader.GetString(1), searchMovieReader.GetDateTime(2), searchMovieReader.GetString(3), Convert.ToInt32(searchMovieReader.GetDouble(4)), searchMovieReader.GetString(5), searchMovieReader.GetString(6), (byte[])searchMovieReader[7]));
                 }
                 catch (Exception e)
@@ -98,12 +96,26 @@ namespace Movie_Database_System.Controllers.Movie
         }
 
         [HttpPost]
-
-        public async Task<IActionResult> AddMovie(Movie_Database_System.Models.ViewModels.AddMovieVM movieVM, Movie_Database_System.Models.ViewModels.AddDirectorVM directorVM)
+        public async Task<IActionResult> AddSuccessfull(Movie_Database_System.Models.ViewModels.AddMovieVM movieVM, Movie_Database_System.Models.ViewModels.AddDirectorVM directorVM)
         {
             Movie_Database_System.Models.Movie newMovie = movieVM;
             string summarySingleQuoted = newMovie.summary;
             newMovie.summary = newMovie.summary.Replace("\'", "\'\'");
+            string dateAsStr = newMovie.date.ToShortDateString();
+            if (dateAsStr.Contains('.'))
+            {
+                dateAsStr = dateAsStr.Replace('.', '/');
+            }
+            else if (dateAsStr.Contains('-'))
+            {
+                dateAsStr = dateAsStr.Replace('-', '/');
+            }
+            string[] dateSplitted = dateAsStr.Split('/');
+            if (Int32.Parse(dateSplitted[1]) > 12)
+            {
+                dateAsStr = dateSplitted[1] + "/" + dateSplitted[0] + "/" + dateSplitted[2];
+            }
+
             Movie_Database_System.Models.Director newDirector = directorVM;
 
             string dataFolderPath = "Data;MovieImages";
@@ -183,8 +195,8 @@ namespace Movie_Database_System.Controllers.Movie
                 var command3 = new SqlCommand("addMovie", connection);
 
                 command3.CommandType = System.Data.CommandType.StoredProcedure;
-                command3.Parameters.Add("@name", System.Data.SqlDbType.NVarChar, 30).Value = newMovie.name;
-                command3.Parameters.Add("@date", System.Data.SqlDbType.NVarChar, 10).Value = newMovie.date.ToShortDateString().Replace('/', '-');
+                command3.Parameters.Add("@name", System.Data.SqlDbType.NVarChar, 50).Value = newMovie.name;
+                command3.Parameters.Add("@date", System.Data.SqlDbType.NVarChar, 10).Value = dateAsStr;
                 command3.Parameters.Add("@genre", System.Data.SqlDbType.NVarChar, 15).Value = newMovie.genre;
                 command3.Parameters.Add("@rating", System.Data.SqlDbType.Float).Value = (float)newMovie.rating;
                 command3.Parameters.Add("@directorid", System.Data.SqlDbType.Int).Value = idDirector;
@@ -203,10 +215,7 @@ namespace Movie_Database_System.Controllers.Movie
                 command4.Parameters.Add("@movieid", System.Data.SqlDbType.Int).Value = newMovieId;
 
                 command4.ExecuteNonQuery();
-
                 connection.Close();
-                System.IO.File.Delete(filePath);
-
             }
             catch (Exception err)
             {
@@ -214,7 +223,9 @@ namespace Movie_Database_System.Controllers.Movie
                 return Json(err.ToString());
             }
 
-            return Json(newMovie);
+            ViewData["ImageSrc"] = "~/Data/MovieImages/" + movieVM.image.FileName;
+            ViewData["NewMovie"] = newMovie;
+            return View();
         }
     }
 }
